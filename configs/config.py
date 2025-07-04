bae1d2e1772bf4a328fd05546279963c2c84f163
root <cc.chaocui@gmail.com>
2025-07-04 06:42:53 +0000

    train from cmd

--- configs/config.py
+++ configs/config.py
@@ -6,6 +6,7 @@ import shutil
 from multiprocessing import cpu_count
 
 import torch
+from train_pipeline.utils import HParams
 
 try:
     import intel_extension_for_pytorch as ipex  # pylint: disable=import-error, unused-import
@@ -57,6 +58,7 @@ class Config:
             self.noparallel,
             self.noautoopen,
             self.dml,
+            self.hparams
         ) = self.arg_parse()
         self.instead = ""
         self.preprocess_per = 3.7
@@ -93,7 +95,162 @@ class Config:
             action="store_true",
             help="torch_dml",
         )
+
+        #define API arguments
+        parser.add_argument(
+        "-singer",
+        "--singer_name",
+        type=str,
+        required=True,
+        help="The singer name" # this is used to generate the experimentation dir to save the training results.
+        )
+        parser.add_argument(
+            "-sr", 
+            "--sampling_rate", 
+            type=str,
+            default='48k',
+            help="Sampling rate"
+        )
+        parser.add_argument(
+            "-f0",
+            "--if_f0_guidance",
+            type=int,
+            default=1,
+            help="Use f0 guidance(required for singing)" 
+        )
+        parser.add_argument(
+            "-tdd",
+            "--training_data_dir",
+            type=str,
+            required=True,
+            help="The directory of the training data"
+        )
+        parser.add_argument(
+            "-spk_id",
+            "--speaker_id",
+            type=int,
+            required=True,
+            help="The speaker id"
+        )
+        parser.add_argument(
+            "-cur",
+            "--cpu_usage_rate",
+            type=float,
+            default=0.6,
+            help="The cpu usage rate" #This is used to generate how many cpu processes to be used when extract f0
+        )
+        parser.add_argument(
+            "-f0m",
+            "--f0_model",
+            type=str,
+            default="rmvpe_gpu",
+            help="The f0 model to be used" #available models:  "pm", "harvest", "dio", "rmvpe", "rmvpe_gpu"
+        )
+        parser.add_argument(
+            "-scf",
+            "--saving_checkpoint_frequency",
+            type=int,
+            default=20,
+            help="The frequency to save the checkpoint file" 
+        )
+        parser.add_argument(
+            "-te",
+            "--total_epoch",
+            type=int,
+            default=100,
+            help="The total number of epoch" 
+        )
+        parser.add_argument(
+            "-bs",
+            "--batch_size",
+            type=int,
+            default=20,
+            help="The batch size in each epoch" 
+        )
+        parser.add_argument(
+            "-slco",
+            "--save_latest_checkpoint_only",
+            type=int,
+            default=1,
+            help="Whether ONLY save the lastest checkpoint" #to save the space
+        )
+        parser.add_argument(
+            "-bmgp",
+            "--base_model_G_path",
+            type=str,
+            default="assets/pretrained_v2/f0G40k.pth",
+            help="The base model G path, the model should be downloaded to this path." 
+        )
+        parser.add_argument(
+            "-bmdp",
+            "--base_model_D_path",
+            type=str,
+            default="assets/pretrained_v2/f0D40k.pth",
+            help="The base model D path, the model should be downloaded to this path." 
+        )
+        parser.add_argument(
+            "-gpus",
+            "--gpus_to_use",
+            type=str,
+            required=True,
+            help="Specify the gpus to use, if not given, use all. (e.g. 0-1-2, uses gpu 0, 1 and 2)" 
+        )
+        parser.add_argument(
+            "--cache_training_data_to_gpu",
+            type=int,
+            default=0,
+            help="Cache the training data in GPU VRAM for faster training. It would probably ONLY helps if training dataset is less than 10 minuites. Large training data size might not worth" 
+        )
+        parser.add_argument(
+            "--save_inference_weights",
+            type=int,
+            default=1,
+            help="Save the weights on every checkpoint to weights directory" 
+        )
+        parser.add_argument(
+            "-v",
+            "--version",
+            type=str,
+            default="v2",
+            help="The base model version." 
+        )
+        parser.add_argument(
+            "--rmvpe_gpus",
+            type=str,
+            required=True,
+            help="How to allocate the gpus when runing rmvpe. e.g. 0-0-1, gpu 0 has 2 processes, and gpu 1 has 1 process. Default to 2 process on each GPU"  #generate the default value by "%s-%s" % (gpus, gpus),
+        )
+
         cmd_opts = parser.parse_args()
+        singer_name = cmd_opts.singer_name
+        # 1. The training config initialized when infer_web.py is executed via config = Config()
+        # 2. As part of step 1, the config in ./config/config.json is copied to ./config/inuse/config.json
+        # 3. After step 2, it load the json file and save the configs to json dict in the config instance. Key is the config versiont/sampling_rate.json
+        # 4. When training, the config with the specific version/sampling_rate.json is dumpped to exp_dir/config.json.
+        # 5. When train.py is executed, the exp_dir/config.json is read into HParams() along with the input arguments passed into the command. 
+        
+        # For this particular parameter parser, I just need to parse the train.py parameters and pass it into train.py.
+        # As long as this parser happens after the step 1 to 5 mentioned above, the training should work out of box.
+        hparams = HParams()
+        #train1key exp_dir1
+        hparams.exp_dir1 = singer_name
+        hparams.sr2 = cmd_opts.sampling_rate
+        hparams.if_f0_3 = cmd_opts.if_f0_guidance
+        hparams.trainset_dir4 = cmd_opts.training_data_dir
+        hparams.spk_id5 = cmd_opts.speaker_id
+        hparams.np7 = cmd_opts.cpu_usage_rate
+        hparams.f0method8 = cmd_opts.f0_model
+        hparams.save_epoch10 = cmd_opts.saving_checkpoint_frequency
+        hparams.total_epoch11 = cmd_opts.total_epoch
+        hparams.batch_size12 = cmd_opts.batch_size
+        hparams.if_save_latest13 = cmd_opts.save_latest_checkpoint_only
+        hparams.pretrained_G14 = cmd_opts.base_model_G_path
+        hparams.pretrained_D15 = cmd_opts.base_model_G_path
+        hparams.gpus16 = cmd_opts.gpus_to_use
+        hparams.if_cache_gpu17 = cmd_opts.cache_training_data_to_gpu
+        hparams.if_save_every_weights18 = cmd_opts.save_inference_weights
+        hparams.version19 = cmd_opts.version
+        hparams.gpus_rmvpe = cmd_opts.rmvpe_gpus
 
         cmd_opts.port = cmd_opts.port if 0 <= cmd_opts.port <= 65535 else 7865
 
@@ -104,6 +261,7 @@ class Config:
             cmd_opts.noparallel,
             cmd_opts.noautoopen,
             cmd_opts.dml,
+            hparams
         )
 
     # has_mps is only available in nightly pytorch (for now) and MasOS 12.3+.
