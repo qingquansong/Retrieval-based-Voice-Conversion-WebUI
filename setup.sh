df2942dcb4c90ae019b00730fe032dff543b480c
root <cc.chaocui@gmail.com>
2025-07-05 05:53:21 +0000

    --allow-empty

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 export SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=True
 
-pip install -r /home/jobuser/qq/Retrieval-based-Voice-Conversion-WebUI/requirements.txt --index-url=https://pypi.org/simple
+pip install -r /home/jobuser/qq/Retrieval-based-Voice-Conversion-WebUI/requirements.txt --index-url=https://pypi.org/simple 
