009cff136fc9315dc5a08d350f87ff1e14cce136
root <cc.chaocui@gmail.com>
2025-07-14 04:48:31 +0000

    initial version of workflow

--- train_cmd.sh
+++ train_cmd.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python train_api.py --singer_name jennie --gpus 0 --rmvpe_gpus 0 --training_data_dir /workspace/training_data/jennie --speaker_id 1
+python train_api.py --singer_name jennie --gpus 0 --rmvpe_gpus 0 --training_data_dir /workspace/training_data/jennie/vocal/clean_vocal --speaker_id 1
